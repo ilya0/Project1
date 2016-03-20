7a3ec87edcb72b2b53bef5e91445e43a1d940576
ilya osovets <iosovets@gmail.com>
2016-03-20 15:54:15 -0700

    started on intial layout

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a game based on the the ancient game of game where you have to game the game 
+when your playing the game, dont forget to 
---
+++ app.js
@@ -0,0 +1,29 @@
+//java script is giving the game logic
+
+console.log("javascript is linked");
+
+var charconstructor = new newCharCard(name,charHp,charHealth){
+  this.name;
+  this.charHp;
+  this.charHealth;
+
+};
+
+var attrCreator = new attributeCard(attrName,attrHp,attrHealth){
+  this.attrName;
+  this.attrHp;
+  this.attrHealth;
+};
+
+
+var attackTurn = function(){
+};
+
+var attrdisbursmentTurn = function(){
+};
+
+var chardisbursmentTurn = function(){
+};
+
+
+
