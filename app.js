ee6d148d2266f33d3b874d50431eadfdb941082e
ilya osovets <iosovets@gmail.com>
2016-03-21 10:56:18 -0700

    Morning char creator issues worked on

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 //java script is giving the game logic
 
-console.log("javascript is linked");
+// var instructor = function(name, username) {
+//   return {
+//     firstName: name,
+//     github: username,
+//     speak: function() {
+//       console.log('Hello');
+//     };
+//   };
+// };
 
-var charconstructor = new newCharCard(name,charHp,charHealth){
-  this.name;
-  this.charHp;
-  this.charHealth;
+// var uncle = instructor('Phil', 'phlco');
+// var me = instructor('Matt', 'fatchicken007');
+
+var newCharCard = function(nameValue,charHpValue,charHealthValue){
+
+  this.nameKey = nameValue;
+  this.charHpKey = charHpValue;
+  this.charHealthKey = charHealthValue;
+
 
 };
+//char create test
+p1char = new newCharCard();
 
-var attrCreator = new attributeCard(attrName,attrHp,attrHealth){
-  this.attrName;
-  this.attrHp;
-  this.attrHealth;
+var attrCreator = function(attrName,attrHp,attrHealth){
+  this.attrNameKey =attrNamevalue;
+  this.attrHpKey = attrHpvalue;
+  this.attrHealthKey = attrHealthvalue;
 };
 
+p1char = new newCharCard();
 
 var attackTurn = function(){
 };
@@ -24,6 +40,8 @@ var attrdisbursmentTurn = function(){
 
 var chardisbursmentTurn = function(){
 };
+
+
 
 
 
