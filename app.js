99433306a93d91bd574dfa848bda741de83e739a
ilya osovets <iosovets@gmail.com>
2016-03-22 17:19:26 -0700

    Added functionality such as buttons and click on actions

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var testdatabaseAttrs = [
-{Attribute:"attr",name:"Black Belt",health:0,hp:1},
+{Attribute:"attr",
+name:"Black Belt",
+health:0,
+hp:1},
+
 {Attribute:"attr",name:"purple belt",health:0,hp:1},
 {Attribute:"attr",name:"Black2 Belt",health:0,hp:1},
 {Attribute:"attr",name:"polk dot belt",health:0,hp:1}];
@@ -8,17 +12,25 @@ var testdatabaseChars = [
 {Attribute:"Char",name:"Clu",health:1,hp:1},
 {Attribute:"Char",name:"Tron",health:1,hp:1},
 {Attribute:"Char",name:"Sark",health:1,hp:1},
-{Attribute:"Char",name:"Neo",health:1,hp:1}
+{Attribute:"Char",name:"Neo",health:1,hp:1},
+{Attribute:"Char",name:"Soul",health:1,hp:1}
 ];
 
-var p1charhand = [{Attribute:"Char",name:"Clu",health:1,hp:1}];
+var p1charhand = [{Attribute:"Char",name:"Clu",health:1,hp:1},
+{Attribute:"Char",name:"Tron",health:1,hp:1},
+{Attribute:"Char",name:"Sark",health:1,hp:1},
+{Attribute:"Char",name:"Neo",health:1,hp:1},
+{Attribute:"Char",name:"Soul",health:1,hp:1}
+];
+
+
 var p2charhand = [{Attribute:"Char",name:"Tron",health:1,hp:1}];
 
 // var p1attrhand = [];
 // var p2attrhand = [];
 
-var p1attackChar = [{Attribute:"Char",name:"Clu",health:1,hp:1}];
-var p2attackChar = [{Attribute:"Char",name:"Tron",health:1,hp:1}];
+var p1attackChar = [];
+var p2attackChar = [];
 
 var AttackTurn = 0;
 
@@ -42,21 +54,50 @@ $('.p1charhand').on('click', function(){
 });
 
 
+///disburse cards to players function
+
 $('#disburseCardbutton').on('click',function(){
-  $('.card1text').text("text changed");
-  $('.card2text').text("text changed");
-  $('.card3text').text("text changed");
-  $('.card4text').text("text changed");
-  $('.card5text').text("text changed");
-  $('.card6text').text("text changed");
-  $('.card7text').text("text changed");
-  $('.card8text').text("text changed");
-  $('.card9text').text("text changed");
-  $('.card10text').text("text changed");
+  $('.card1text').text(p1charhand[0].name,p1charhand[0].hp);
+  $('.card2text').text(p1charhand[1].name,p1charhand[1].hp);
+  $('.card3text').text(p1charhand[2].name,p1charhand[2].hp);
+  $('.card4text').text(p1charhand[3].name,p1charhand[3].hp);
+  $('.card5text').text(p1charhand[4].name,p1charhand[4].hp);
+  $('.card6text').text(p2charhand[0].name,p2charhand[0].hp);
+  $('.card7text').text(p2charhand[1].name,p2charhand[1].hp);
+  $('.card8text').text(p2charhand[2].name,p2charhand[2].hp);
+  $('.card9text').text(p2charhand[3].name,p2charhand[3].hp);
+  $('.card10text').text(p2charhand[4].name,p2charhand[4].hp);
 
 
 });
 
+///select card for attack function
+
+$('#card1').on('click', function() {
+  console.log("attack char added from p1 [0]");
+ var cell1 = $(this).text();
+ p1attackChar.push(p1charhand[0]);
+});
+$('.card2').on('click', function() {
+   console.log("attack char added from p1 [1]");
+ var cell1 = $(this).text();
+ p1attackChar.push(p1charhand[1]);
+});
+$('.card3').on('click', function() {
+   console.log("attack char added from p1 [2]");
+ var cell1 = $(this).text();
+ p1attackChar.push(p1charhand[2]);
+});
+$('.card4').on('click', function() {
+   console.log("attack char added from p1 [3]");
+ var cell1 = $(this).text();
+ p1attackChar.push(p1charhand[3]);
+});
+$('.card5').on('click', function() {
+   console.log("attack char added from p1 [4]");
+ var cell1 = $(this).text();
+ p1attackChar.push(p1charhand[4]);
+});
 
 
 
