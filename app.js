3ed772a97287817c1f99b943801e3007b39f0db6
ilya osovets <iosovets@gmail.com>
2016-03-23 21:49:52 -0700

    Added movement functionality, game closes to end, needs final cgame ending logic

--- app.js
+++ app.js
@@ -1,31 +1,19 @@
-// var testdatabaseAttrs = [
-// {Attribute:"attr",name:"Black Belt",health:0,hp:1},
-// {Attribute:"attr",name:"purple belt",health:0,hp:1},
-// {Attribute:"attr",name:"Black2 Belt",health:0,hp:1},
-// {Attribute:"attr",name:"polk dot belt",health:0,hp:1}
-//];
-
-var testdatabaseChars = [
-  {Attribute:"Char",name:"Clu",health:1,hp:1},
-  {Attribute:"Char",name:"Tron",health:1,hp:1},
-  {Attribute:"Char",name:"Sark",health:1,hp:1},
-  {Attribute:"Char",name:"Neo",health:1,hp:1},
-  {Attribute:"Char",name:"Soul",health:1,hp:1}
+//neo and ron chars are giving back loads into p1attack char
+var testdatabaseChars = [{Attribute:"Char",name:"Clu",health:1,hp:1,cardis:"card99"},
+{Attribute:"Char",name:"Tron",health:1,hp:1,cardis:"card99"},
+{Attribute:"Char",name:"Sark",health:1,hp:1,cardis:"card99"},
+{Attribute:"Char",name:"Neo",health:1,hp:1,cardis:"card99"},
+{Attribute:"Char",name:"Soul",health:1,hp:1,cardis:"card99"},
+{Attribute:"Char",name:"BON",health:1,hp:1,cardis:"card99"},
+{Attribute:"Char",name:"CON",health:1,hp:1,cardis:"card99"},
+{Attribute:"Char",name:"Son",health:1,hp:1,cardis:"card99"},
+{Attribute:"Char",name:"LON",health:1,hp:1,cardis:"card99"},
+{Attribute:"Char",name:"RON",health:1,hp:1,cardis:"card99"}
 ];
 
 //char hands for characters
-var p1charhand = [{Attribute:"Char",name:"Clu",health:1,hp:1},
-{Attribute:"Char",name:"Tron",health:1,hp:1},
-{Attribute:"Char",name:"Sark",health:1,hp:1},
-{Attribute:"Char",name:"Neo",health:1,hp:1},
-{Attribute:"Char",name:"Soul",health:1,hp:1}
-];
-var p2charhand = [{Attribute:"Char",name:"BON",health:1,hp:1},
-{Attribute:"Char",name:"CON",health:1,hp:1},
-{Attribute:"Char",name:"Son",health:1,hp:1},
-{Attribute:"Char",name:"LON",health:1,hp:1},
-{Attribute:"Char",name:"RON",health:1,hp:1}
-];
+var p1charhand = [];
+var p2charhand = [];
 
 // hard code p2 hand
 
@@ -34,6 +22,9 @@ var p1attackChar = ["p1 attack char undefined"];
 //p1attackchar is defined with the click on the card
 var p2attackChar = ["p2 attack char undefined"];
 //p2 is hard coded for testing purposes
+
+// var to remove the card in the card remove function
+var removeCardID ="undefined";
 
 
 //**Game mode intialisers
@@ -61,17 +52,30 @@ $('.card').hide();
 
 ///disburse cards to players function
 $('#disburseCardbutton').on('click',function(){
+  disburser(testdatabaseChars,p1charhand,p2charhand);
+
   $('.card1text').html(p1charhand[0].name + p1charhand[0].hp); /// redo with Html
+  p1charhand[0].cardis = "#card1";
   $('.card2text').text(p1charhand[1].name,p1charhand[1].hp);
+  p1charhand[1].cardis = "#card2";
   $('.card3text').text(p1charhand[2].name,p1charhand[2].hp);
+  p1charhand[2].cardis = "#card3";
   $('.card4text').text(p1charhand[3].name,p1charhand[3].hp);
+  p1charhand[3].cardis = "#card4";
   $('.card5text').text(p1charhand[4].name,p1charhand[4].hp);
+  p1charhand[4].cardis = "#card5";
   $('.card6text').text(p2charhand[0].name,p2charhand[0].hp);
+  p2charhand[0].cardis = "#card6";
   $('.card7text').text(p2charhand[1].name,p2charhand[1].hp);
+  p2charhand[1].cardis = "#card7";
   $('.card8text').text(p2charhand[2].name,p2charhand[2].hp);
+  p2charhand[2].cardis = "#card8";
   $('.card9text').text(p2charhand[3].name,p2charhand[3].hp);
+  p2charhand[3].cardis = "#card9";
   $('.card10text').text(p2charhand[4].name,p2charhand[4].hp);
+  p2charhand[4].cardis = "#card10";
   $('.card').show();
+
 });
 
 
@@ -96,6 +100,7 @@ $('#card1').on('click', function() {
 $('#card2').on('click', function() {
 console.log("attack char added from p1 [1]");
   p1attackChar.pop();
+
     //remove active attack card
   $('.card.player1').appendTo($('#p1attackstack'));
   p1attackChar.push(p1charhand[1]);
@@ -109,6 +114,7 @@ console.log("attack char added from p1 [1]");
 $('#card3').on('click', function() {
    console.log("attack char added from p1 [2]");
   p1attackChar.pop();
+
     //remove active attack card
   $('.card.player1').appendTo($('#p1attackstack'));
   p1attackChar.push(p1charhand[2]);
@@ -122,6 +128,7 @@ $('#card3').on('click', function() {
 $('#card4').on('click', function() {
    console.log("attack char added from p1 [3]");
  p1attackChar.pop();
+
    //remove active attack card
   $('.card.player1').appendTo($('#p1attackstack'));
   p1attackChar.push(p1charhand[3]);
@@ -135,6 +142,7 @@ $('#card4').on('click', function() {
 $('#card5').on('click', function() {
    console.log("attack char added from p1 [4]");
  p1attackChar.pop();
+
    //remove active attack card
   $('.card.player1').appendTo($('#p1attackstack'));
   p1attackChar.push(p1charhand[4]);
@@ -146,7 +154,7 @@ $('#card5').on('click', function() {
 //////*****P2 Cards stack
 //card 6 click action
 $('#card6').on('click', function() {
-   console.log("attack char added from p2 [4]");
+   console.log("attack char added from p2 [0]");
  p2attackChar.pop();
    //remove active attack card
   $('.card.player2').appendTo($('#p2attackstack'));
@@ -158,18 +166,18 @@ $('#card6').on('click', function() {
 
 //card 7 click action
 $('#card7').on('click', function() {
-   console.log("attack char added from p2 [4]");
+   console.log("attack char added from p2 [1]");
  p2attackChar.pop();
    //remove active attack card
   $('.card.player2').appendTo($('#p2attackstack'));
-  p1attackChar.push(p2charhand[1]);
+  p2attackChar.push(p2charhand[1]);
   $( "#card7" ).appendTo( $( "#attackboard" ) );
   console.log("ATTACK Char object - " + p2attackChar[0]);
 });
 
 //card 8 click action
 $('#card8').on('click', function() {
-   console.log("attack char added from p2 [4]");
+   console.log("attack char added from p2 [2]");
  p2attackChar.pop();
    //remove active attack card
   $('.card.player2').appendTo($('#p2attackstack'));
@@ -180,7 +188,7 @@ $('#card8').on('click', function() {
 
 //card 9 click action
 $('#card9').on('click', function() {
-   console.log("attack char added from p2 [4]");
+   console.log("attack char added from p2 [3]");
  p2attackChar.pop();
    //remove active attack card
   $('.card.player2').appendTo($('#p2attackstack'));
@@ -229,6 +237,8 @@ var attackTurn = function(){
   $('.card.player1').appendTo($('#p1attackstack'));
   removeChar(p1charhand);
   removeChar(p2charhand);
+  p1attackChar = ["reset after attack"];
+  p2attackChar = ["reset after attack"];
 };
 
 
@@ -297,16 +307,24 @@ var times=2;
 //***** Win eval function
 var winEval = function(){
 
+
 };
 
 ///*** Remove char function
-var removeChar = function(){
+var removeChar = function(array){
+
+
   // if char health =0, remove from array completely
+
     for(var i = 0; i < array.length; i++){
     if(array[i].health <= 0){
+        removeCardID = array[i].cardis;
+        $(removeCardID).remove();
+        alert(array[i].name,"has been removed");
+      //array.splice(i,1);
+      array[i] = 0;
 
-      array.splice(i,1);
-      alert(array[i].name,"has been removed");
+      //remove player card that was represented by t
     }
     else{
       //alert("no chars removed");
